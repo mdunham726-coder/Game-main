0df38848ee511fa645a6d71fb5b26b238dfe5395
mdunham726-coder <mdunham726@gmail.com>
2026-04-01 00:49:43 -0400

    patch

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static(__dirname));
 
 // Session state management
 const sessionStates = new Map();
